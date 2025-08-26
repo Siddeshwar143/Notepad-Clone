9288e1848c1a7812bcae667b5cb9b28ad9be6806
Siddeshwar <siddeshwarmalige5@gmail.com>
2025-08-26 20:16:39 +0530

    Add files via upload

---
+++ NotePadProMax.Designer.cs
@@ -0,0 +1,390 @@
+﻿namespace Day36
+{
+    partial class NotePadProMax
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new MenuStrip();
+            fileToolStripMenuItem = new ToolStripMenuItem();
+            newTabToolStripMenuItem = new ToolStripMenuItem();
+            openToolStripMenuItem = new ToolStripMenuItem();
+            toolStripSeparator4 = new ToolStripSeparator();
+            saveToolStripMenuItem = new ToolStripMenuItem();
+            saveAsToolStripMenuItem = new ToolStripMenuItem();
+            saveAllToolStripMenuItem = new ToolStripMenuItem();
+            toolStripSeparator2 = new ToolStripSeparator();
+            pageSetupToolStripMenuItem = new ToolStripMenuItem();
+            printToolStripMenuItem = new ToolStripMenuItem();
+            toolStripSeparator3 = new ToolStripSeparator();
+            exitToolStripMenuItem = new ToolStripMenuItem();
+            editToolStripMenuItem = new ToolStripMenuItem();
+            undoToolStripMenuItem = new ToolStripMenuItem();
+            redoToolStripMenuItem = new ToolStripMenuItem();
+            toolStripSeparator1 = new ToolStripSeparator();
+            cutToolStripMenuItem = new ToolStripMenuItem();
+            copyToolStripMenuItem = new ToolStripMenuItem();
+            pastToolStripMenuItem = new ToolStripMenuItem();
+            deleteToolStripMenuItem = new ToolStripMenuItem();
+            toolStripSeparator5 = new ToolStripSeparator();
+            selectAllToolStripMenuItem = new ToolStripMenuItem();
+            toolStripSeparator6 = new ToolStripSeparator();
+            forntToolStripMenuItem = new ToolStripMenuItem();
+            viewToolStripMenuItem = new ToolStripMenuItem();
+            zoomToolStripMenuItem = new ToolStripMenuItem();
+            zoomInToolStripMenuItem = new ToolStripMenuItem();
+            zoomOutToolStripMenuItem = new ToolStripMenuItem();
+            restoreDefultZoomToolStripMenuItem = new ToolStripMenuItem();
+            statusBarToolStripMenuItem = new ToolStripMenuItem();
+            wordToolStripMenuItem = new ToolStripMenuItem();
+            fontDialog1 = new FontDialog();
+            saveFileDialog1 = new SaveFileDialog();
+            openFileDialog1 = new OpenFileDialog();
+            txtWords = new RichTextBox();
+            backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            statusStrip1 = new StatusStrip();
+            folderBrowserDialog1 = new FolderBrowserDialog();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.BackColor = Color.Gainsboro;
+            menuStrip1.ImageScalingSize = new Size(20, 20);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem, editToolStripMenuItem, viewToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 28);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            fileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { newTabToolStripMenuItem, openToolStripMenuItem, toolStripSeparator4, saveToolStripMenuItem, saveAsToolStripMenuItem, saveAllToolStripMenuItem, toolStripSeparator2, pageSetupToolStripMenuItem, printToolStripMenuItem, toolStripSeparator3, exitToolStripMenuItem });
+            fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            fileToolStripMenuItem.Size = new Size(46, 24);
+            fileToolStripMenuItem.Text = "&File";
+            // 
+            // newTabToolStripMenuItem
+            // 
+            newTabToolStripMenuItem.Name = "newTabToolStripMenuItem";
+            newTabToolStripMenuItem.ShortcutKeys = Keys.Control | Keys.N;
+            newTabToolStripMenuItem.Size = new Size(231, 26);
+            newTabToolStripMenuItem.Text = "New tab";
+            newTabToolStripMenuItem.Click += newTabToolStripMenuItem_Click;
+            // 
+            // openToolStripMenuItem
+            // 
+            openToolStripMenuItem.Name = "openToolStripMenuItem";
+            openToolStripMenuItem.ShortcutKeys = Keys.Control | Keys.O;
+            openToolStripMenuItem.Size = new Size(231, 26);
+            openToolStripMenuItem.Text = "Open";
+            openToolStripMenuItem.Click += openToolStripMenuItem_Click;
+            // 
+            // toolStripSeparator4
+            // 
+            toolStripSeparator4.Name = "toolStripSeparator4";
+            toolStripSeparator4.Size = new Size(228, 6);
+            // 
+            // saveToolStripMenuItem
+            // 
+            saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            saveToolStripMenuItem.ShortcutKeys = Keys.Control | Keys.S;
+            saveToolStripMenuItem.Size = new Size(231, 26);
+            saveToolStripMenuItem.Text = "Save";
+            saveToolStripMenuItem.Click += saveToolStripMenuItem_Click;
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            saveAsToolStripMenuItem.ShortcutKeys = Keys.Control | Keys.Shift | Keys.S;
+            saveAsToolStripMenuItem.Size = new Size(231, 26);
+            saveAsToolStripMenuItem.Text = "Save as";
+            saveAsToolStripMenuItem.Click += saveAsToolStripMenuItem_Click;
+            // 
+            // saveAllToolStripMenuItem
+            // 
+            saveAllToolStripMenuItem.Name = "saveAllToolStripMenuItem";
+            saveAllToolStripMenuItem.ShortcutKeys = Keys.Control | Keys.Alt | Keys.S;
+            saveAllToolStripMenuItem.Size = new Size(231, 26);
+            saveAllToolStripMenuItem.Text = "Save all";
+            saveAllToolStripMenuItem.Click += saveAllToolStripMenuItem_Click;
+            // 
+            // toolStripSeparator2
+            // 
+            toolStripSeparator2.Name = "toolStripSeparator2";
+            toolStripSeparator2.Size = new Size(228, 6);
+            // 
+            // pageSetupToolStripMenuItem
+            // 
+            pageSetupToolStripMenuItem.Name = "pageSetupToolStripMenuItem";
+            pageSetupToolStripMenuItem.Size = new Size(231, 26);
+            pageSetupToolStripMenuItem.Text = "Page setup";
+            pageSetupToolStripMenuItem.Click += pageSetupToolStripMenuItem_Click;
+            // 
+            // printToolStripMenuItem
+            // 
+            printToolStripMenuItem.Name = "printToolStripMenuItem";
+            printToolStripMenuItem.ShortcutKeys = Keys.Control | Keys.P;
+            printToolStripMenuItem.Size = new Size(231, 26);
+            printToolStripMenuItem.Text = "Print";
+            printToolStripMenuItem.Click += printToolStripMenuItem_Click;
+            // 
+            // toolStripSeparator3
+            // 
+            toolStripSeparator3.Name = "toolStripSeparator3";
+            toolStripSeparator3.Size = new Size(228, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            exitToolStripMenuItem.Size = new Size(231, 26);
+            exitToolStripMenuItem.Text = "Exit";
+            exitToolStripMenuItem.Click += exitToolStripMenuItem_Click;
+            // 
+            // editToolStripMenuItem
+            // 
+            editToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { undoToolStripMenuItem, redoToolStripMenuItem, toolStripSeparator1, cutToolStripMenuItem, copyToolStripMenuItem, pastToolStripMenuItem, deleteToolStripMenuItem, toolStripSeparator5, selectAllToolStripMenuItem, toolStripSeparator6, forntToolStripMenuItem });
+            editToolStripMenuItem.Name = "editToolStripMenuItem";
+            editToolStripMenuItem.Size = new Size(49, 24);
+            editToolStripMenuItem.Text = "&Edit";
+            // 
+            // undoToolStripMenuItem
+            // 
+            undoToolStripMenuItem.Name = "undoToolStripMenuItem";
+            undoToolStripMenuItem.ShortcutKeys = Keys.Control | Keys.Z;
+            undoToolStripMenuItem.Size = new Size(204, 26);
+            undoToolStripMenuItem.Text = "Undo";
+            undoToolStripMenuItem.Click += undoToolStripMenuItem_Click;
+            // 
+            // redoToolStripMenuItem
+            // 
+            redoToolStripMenuItem.Name = "redoToolStripMenuItem";
+            redoToolStripMenuItem.ShortcutKeys = Keys.Control | Keys.Y;
+            redoToolStripMenuItem.Size = new Size(204, 26);
+            redoToolStripMenuItem.Text = "Redo";
+            redoToolStripMenuItem.Click += redoToolStripMenuItem_Click;
+            // 
+            // toolStripSeparator1
+            // 
+            toolStripSeparator1.Name = "toolStripSeparator1";
+            toolStripSeparator1.Size = new Size(201, 6);
+            // 
+            // cutToolStripMenuItem
+            // 
+            cutToolStripMenuItem.Name = "cutToolStripMenuItem";
+            cutToolStripMenuItem.ShortcutKeys = Keys.Control | Keys.X;
+            cutToolStripMenuItem.Size = new Size(204, 26);
+            cutToolStripMenuItem.Text = "Cut";
+            cutToolStripMenuItem.Click += cutToolStripMenuItem_Click;
+            // 
+            // copyToolStripMenuItem
+            // 
+            copyToolStripMenuItem.Name = "copyToolStripMenuItem";
+            copyToolStripMenuItem.ShortcutKeys = Keys.Control | Keys.C;
+            copyToolStripMenuItem.Size = new Size(204, 26);
+            copyToolStripMenuItem.Text = "Copy";
+            copyToolStripMenuItem.Click += copyToolStripMenuItem_Click;
+            // 
+            // pastToolStripMenuItem
+            // 
+            pastToolStripMenuItem.Name = "pastToolStripMenuItem";
+            pastToolStripMenuItem.ShortcutKeys = Keys.Control | Keys.V;
+            pastToolStripMenuItem.Size = new Size(204, 26);
+            pastToolStripMenuItem.Text = "Past";
+            pastToolStripMenuItem.Click += pastToolStripMenuItem_Click;
+            // 
+            // deleteToolStripMenuItem
+            // 
+            deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+            deleteToolStripMenuItem.ShortcutKeys = Keys.Delete;
+            deleteToolStripMenuItem.Size = new Size(204, 26);
+            deleteToolStripMenuItem.Text = "Delete";
+            deleteToolStripMenuItem.Click += deleteToolStripMenuItem_Click;
+            // 
+            // toolStripSeparator5
+            // 
+            toolStripSeparator5.Name = "toolStripSeparator5";
+            toolStripSeparator5.Size = new Size(201, 6);
+            // 
+            // selectAllToolStripMenuItem
+            // 
+            selectAllToolStripMenuItem.Name = "selectAllToolStripMenuItem";
+            selectAllToolStripMenuItem.ShortcutKeys = Keys.Control | Keys.A;
+            selectAllToolStripMenuItem.Size = new Size(204, 26);
+            selectAllToolStripMenuItem.Text = "Select all";
+            selectAllToolStripMenuItem.Click += selectAllToolStripMenuItem_Click;
+            // 
+            // toolStripSeparator6
+            // 
+            toolStripSeparator6.Name = "toolStripSeparator6";
+            toolStripSeparator6.Size = new Size(201, 6);
+            // 
+            // forntToolStripMenuItem
+            // 
+            forntToolStripMenuItem.Name = "forntToolStripMenuItem";
+            forntToolStripMenuItem.Size = new Size(204, 26);
+            forntToolStripMenuItem.Text = "Fornt";
+            forntToolStripMenuItem.Click += forntToolStripMenuItem_Click;
+            // 
+            // viewToolStripMenuItem
+            // 
+            viewToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { zoomToolStripMenuItem, statusBarToolStripMenuItem, wordToolStripMenuItem });
+            viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            viewToolStripMenuItem.Size = new Size(55, 24);
+            viewToolStripMenuItem.Text = "&View";
+            // 
+            // zoomToolStripMenuItem
+            // 
+            zoomToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { zoomInToolStripMenuItem, zoomOutToolStripMenuItem, restoreDefultZoomToolStripMenuItem });
+            zoomToolStripMenuItem.Name = "zoomToolStripMenuItem";
+            zoomToolStripMenuItem.Size = new Size(165, 26);
+            zoomToolStripMenuItem.Text = "Zoom";
+            // 
+            // zoomInToolStripMenuItem
+            // 
+            zoomInToolStripMenuItem.Name = "zoomInToolStripMenuItem";
+            zoomInToolStripMenuItem.ShortcutKeys = Keys.Control | Keys.Oemplus;
+            zoomInToolStripMenuItem.Size = new Size(277, 26);
+            zoomInToolStripMenuItem.Text = "Zoom in";
+            zoomInToolStripMenuItem.Click += zoomInToolStripMenuItem_Click;
+            // 
+            // zoomOutToolStripMenuItem
+            // 
+            zoomOutToolStripMenuItem.Name = "zoomOutToolStripMenuItem";
+            zoomOutToolStripMenuItem.ShortcutKeys = Keys.Control | Keys.OemMinus;
+            zoomOutToolStripMenuItem.Size = new Size(277, 26);
+            zoomOutToolStripMenuItem.Text = "Zoom out";
+            zoomOutToolStripMenuItem.Click += zoomOutToolStripMenuItem_Click;
+            // 
+            // restoreDefultZoomToolStripMenuItem
+            // 
+            restoreDefultZoomToolStripMenuItem.Name = "restoreDefultZoomToolStripMenuItem";
+            restoreDefultZoomToolStripMenuItem.ShortcutKeys = Keys.Control | Keys.D0;
+            restoreDefultZoomToolStripMenuItem.Size = new Size(277, 26);
+            restoreDefultZoomToolStripMenuItem.Text = "Restore defult zoom";
+            restoreDefultZoomToolStripMenuItem.Click += restoreDefultZoomToolStripMenuItem_Click;
+            // 
+            // statusBarToolStripMenuItem
+            // 
+            statusBarToolStripMenuItem.Name = "statusBarToolStripMenuItem";
+            statusBarToolStripMenuItem.Size = new Size(165, 26);
+            statusBarToolStripMenuItem.Text = "Status bar";
+            statusBarToolStripMenuItem.Click += statusBarToolStripMenuItem_Click;
+            // 
+            // wordToolStripMenuItem
+            // 
+            wordToolStripMenuItem.Name = "wordToolStripMenuItem";
+            wordToolStripMenuItem.Size = new Size(165, 26);
+            wordToolStripMenuItem.Text = "Word wrap";
+            wordToolStripMenuItem.Click += wordToolStripMenuItem_Click;
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // txtWords
+            // 
+            txtWords.Dock = DockStyle.Fill;
+            txtWords.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtWords.Location = new Point(0, 28);
+            txtWords.Name = "txtWords";
+            txtWords.Size = new Size(800, 422);
+            txtWords.TabIndex = 1;
+            txtWords.Text = "";
+            txtWords.WordWrap = false;
+            // 
+            // statusStrip1
+            // 
+            statusStrip1.ImageScalingSize = new Size(20, 20);
+            statusStrip1.Location = new Point(0, 428);
+            statusStrip1.Name = "statusStrip1";
+            statusStrip1.Size = new Size(800, 22);
+            statusStrip1.TabIndex = 2;
+            statusStrip1.Text = "statusStrip1";
+            // 
+            // NotePadProMax
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(statusStrip1);
+            Controls.Add(txtWords);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "NotePadProMax";
+            StartPosition = FormStartPosition.CenterScreen;
+            Load += NotePadProMax_Load;
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem editToolStripMenuItem;
+        private ToolStripMenuItem viewToolStripMenuItem;
+        private ToolStripMenuItem newTabToolStripMenuItem;
+        private ToolStripMenuItem openToolStripMenuItem;
+        private ToolStripSeparator toolStripSeparator4;
+        private ToolStripMenuItem saveToolStripMenuItem;
+        private ToolStripMenuItem saveAsToolStripMenuItem;
+        private ToolStripMenuItem saveAllToolStripMenuItem;
+        private ToolStripSeparator toolStripSeparator2;
+        private ToolStripMenuItem pageSetupToolStripMenuItem;
+        private ToolStripMenuItem printToolStripMenuItem;
+        private ToolStripSeparator toolStripSeparator3;
+        private ToolStripMenuItem exitToolStripMenuItem;
+        private ToolStripMenuItem undoToolStripMenuItem;
+        private ToolStripMenuItem redoToolStripMenuItem;
+        private ToolStripMenuItem cutToolStripMenuItem;
+        private ToolStripMenuItem copyToolStripMenuItem;
+        private ToolStripMenuItem pastToolStripMenuItem;
+        private ToolStripMenuItem deleteToolStripMenuItem;
+        private ToolStripMenuItem forntToolStripMenuItem;
+        private ToolStripMenuItem selectAllToolStripMenuItem;
+        private ToolStripMenuItem zoomToolStripMenuItem;
+        private ToolStripMenuItem statusBarToolStripMenuItem;
+        private ToolStripMenuItem wordToolStripMenuItem;
+        private ToolStripSeparator toolStripSeparator1;
+        private ToolStripSeparator toolStripSeparator5;
+        private ToolStripSeparator toolStripSeparator6;
+        private ToolStripMenuItem zoomInToolStripMenuItem;
+        private ToolStripMenuItem zoomOutToolStripMenuItem;
+        private ToolStripMenuItem restoreDefultZoomToolStripMenuItem;
+        private RichTextBox txtWords;
+        private FontDialog fontDialog1;
+        private SaveFileDialog saveFileDialog1;
+        private OpenFileDialog openFileDialog1;
+        private RichTextBox richTextBox1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private StatusStrip statusStrip1;
+        private FolderBrowserDialog folderBrowserDialog1;
+    }
+}
---
+++ NotePadProMax.cs
@@ -0,0 +1,191 @@
+﻿using System.Drawing.Printing;
+
+namespace Day36
+{
+    public partial class NotePadProMax : Form
+    {
+
+        float currentZoom = 1.0f;
+        Font defaultFont;
+        public NotePadProMax()
+        {
+            InitializeComponent();
+            defaultFont = txtWords.Font;
+            //this.FormBorderStyle = FormBorderStyle.None; // Removes the border and title bar
+            //this.ControlBox = false;                     // Removes minimize, maximize, and close buttons
+            //this.Text = string.Empty;
+        }
+        private string currentFilePath = "Untitled";
+        private void NotePadProMax_Load(object sender, EventArgs e)
+        {
+            currentFilePath = "Untitled";
+            this.Text = $"{currentFilePath} - NotePadProMax";
+        }
+        private void newTabToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            txtWords.Clear();
+            currentFilePath = "Untitled";
+            this.Text = $"{currentFilePath} - NotePadProMax";
+        }
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.FileName = "";
+            openFileDialog1.Filter = "Text Document|*.txt|All Files(*.*)|*.*";
+            DialogResult dr = openFileDialog1.ShowDialog();
+            if (dr == DialogResult.OK)
+            {
+                string filePath = openFileDialog1.FileName;
+                txtWords.LoadFile(filePath, RichTextBoxStreamType.PlainText);
+                currentFilePath = filePath;
+                this.Text = $"{System.IO.Path.GetFileName(currentFilePath)} - NotePadProMax";
+            }
+        }
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.FileName = "*.txt";
+            saveFileDialog1.Filter = "Text documents|*.txt|All Files|*.*";
+            DialogResult dr = saveFileDialog1.ShowDialog();
+            if (dr == DialogResult.OK)
+            {
+                string filePath = saveFileDialog1.FileName;
+                txtWords.SaveFile(filePath, RichTextBoxStreamType.PlainText);
+                currentFilePath = filePath;
+                this.Text = $"{System.IO.Path.GetFileName(currentFilePath)} - NotePadProMax";
+            }
+        }
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveAsToolStripMenuItem.Name = "";
+            saveFileDialog1.Filter = "Text documents|*.txt|All Files|*.*";
+            DialogResult dr = saveFileDialog1.ShowDialog();
+            if (dr == DialogResult.OK)
+            {
+                string filePath = saveFileDialog1.FileName;
+
+                txtWords.SaveFile(filePath, RichTextBoxStreamType.PlainText);
+                currentFilePath = filePath;
+                this.Text = $"{System.IO.Path.GetFileName(currentFilePath)} - NotePadProMax";
+            }
+        }
+        private void saveAllToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Text documents (*.txt)|*.txt|All Files (*.*)|*.*";
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                txtWords.SaveFile(sfd.FileName, RichTextBoxStreamType.PlainText);
+            }
+        }
+        private void pageSetupToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            PrintDocument printDocument = new PrintDocument();
+            PageSetupDialog pg = new PageSetupDialog();
+
+            pg.Document = printDocument;
+            pg.ShowDialog();
+        }
+        private void printToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            PrintDocument printDocument = new PrintDocument();
+            PrintDialog printDialog = new PrintDialog();
+            printDialog.Document = printDocument;
+
+            if (printDialog.ShowDialog() == DialogResult.OK)
+            {
+                printDocument.Print();
+            }
+        }
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void undoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            txtWords.Undo();
+        }
+        private void redoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            txtWords.Redo();
+        }
+        private void cutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            txtWords.Cut();
+        }
+        private void copyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            txtWords.Copy();
+        }
+        private void pastToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            txtWords.Paste();
+        }
+        private void deleteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int selectionStart = txtWords.SelectionStart;
+            int selectionLength = txtWords.SelectionLength;
+
+            if (selectionLength > 0)
+            {
+                txtWords.Text = txtWords.Text.Remove(selectionStart, selectionLength);
+                txtWords.SelectionStart = selectionStart;
+            }
+        }
+        private void selectAllToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            txtWords.SelectAll();
+        }
+        private void forntToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            fontDialog1.Font = txtWords.Font;
+            DialogResult dr = fontDialog1.ShowDialog();
+            if (dr == DialogResult.OK)
+            {
+                txtWords.Font = fontDialog1.Font;
+            }
+        }
+        private void zoomInToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            currentZoom += 0.1f;
+            txtWords.Font = new Font(txtWords.Font.FontFamily, txtWords.Font.Size * 1.1f);
+        }
+        private void zoomOutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (txtWords.Font.Size > 2)
+            {
+                currentZoom -= 0.1f;
+                txtWords.Font = new Font(txtWords.Font.FontFamily, txtWords.Font.Size / 1.1f);
+            }
+        }
+        private void restoreDefultZoomToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            txtWords.Font = defaultFont;
+            currentZoom = 1.0f;
+        }
+        private void statusBarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            statusBarToolStripMenuItem.Checked = !statusBarToolStripMenuItem.Checked;
+            if (statusBarToolStripMenuItem.Checked)
+            {
+                txtWords.AcceptsTab = true;
+            }
+            else
+            {
+                txtWords.AcceptsTab = false;
+            }
+        }
+        private void wordToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            wordToolStripMenuItem.Checked = !wordToolStripMenuItem.Checked;
+            if (wordToolStripMenuItem.Checked)
+            {
+                txtWords.WordWrap = true;
+            }
+            else
+            {
+                txtWords.WordWrap = false;
+            }
+        }
+    }
+}
